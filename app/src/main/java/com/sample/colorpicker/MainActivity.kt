2f71687e67bbe7c543c06174df2f19bc274fa872
Dev2 <dev2@hypersoft.com.pk>
2024-10-08 14:06:30 +0500

    initial build

--- app/src/main/java/com/sample/colorpicker/MainActivity.kt
+++ app/src/main/java/com/sample/colorpicker/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.sample.colorpicker
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hyeprsoft.ColorPickerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ColorPickerView.OnColorChangeListener {
+
+    private val TAG = "ColorPicker"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        val colorPicker = findViewById<ColorPickerView>(R.id.color_picker)
+
+        colorPicker.setOnColorChangeListener(this)
+        colorPicker.setStrokeColor(ContextCompat.getColor(this,R.color.black)) // change stroke color
+        colorPicker.setStrokeWidth(20f) // min 10f and max 50f
+        colorPicker.setCircleRadius(30f) // min 30f and max 200f
+        colorPicker.setChangeStrokeColorRealTime(true)
+        colorPicker.getColorName("HexColorCode")
+
+    }
+
+    override fun onColorChanged(color: Int) {
+        Log.i(TAG, "onColorChanged: $color")
+    }
+
+    override fun onHexColorChanged(hexColor: String) {
+        Log.i(TAG, "onColorChanged: $hexColor")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "ColorPicker"
 include(":app")
- 
+include(":ColorPicker")
